b16c1bb223de792c2a13d5d509cf1c10593881d0
vanechek777 <glmdv.ivan@gmail.com>
2026-04-04 01:37:31 +0900

    feat(mobile): add registration button and deep linking for MAUI

--- FinanceManager/FinanceManager/Platforms/Android/MainActivity.cs
+++ FinanceManager/FinanceManager/Platforms/Android/MainActivity.cs
@@ -1,10 +1,13 @@
-﻿using Android.App;
+using Android.App;
 using Android.Content.PM;
 using Android.OS;
 
 namespace FinanceManager
 {
-    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, Exported = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    [IntentFilter(new[] { Android.Content.Intent.ActionView },
+        Categories = new[] { Android.Content.Intent.CategoryDefault, Android.Content.Intent.CategoryBrowsable },
+        DataScheme = "financeflow")]
     public class MainActivity : MauiAppCompatActivity
     {
     }
